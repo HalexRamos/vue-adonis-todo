5eeca00a2a5cf2fe9120e0bb36eb752dccab3dce
HalexRamos <halexramos96@gmail.com>
2022-07-10 15:16:16 -0300

    initial vue setup

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,12 @@
+import createPersistedState from 'vuex-persistedstate';
+
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
   },
   getters: {
@@ -14,4 +17,7 @@ export default new Vuex.Store({
   },
   modules: {
   },
+  plugins: [
+    createPersistedState(),
+  ],
 });
